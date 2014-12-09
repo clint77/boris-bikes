7d1c44e61cfc3f89fd0446e5ac9eb61be8fa0371
Clint <clintp1@gmail.com>
2014-12-09 17:49:28 +0000

    refractoring bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,24 +3,23 @@ require 'bike'
 
 #we're describing the functionality of a specific class, Bike
 describe Bike do
-	# this is a specific feature (behaviour)
-	# that we expect to be present
+	
+	let(:bike) { Bike.new }
+	let(:breaking) { bike.break! }
+
 	it "should not be broken after we create it" do
-		the_bike = Bike.new # initialising a new object of Bike class
-		# expect an instance of this class to have
-		# a method "broken?" that should return false
-		expect(the_bike).not_to be_broken
+		expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do
-		bike = Bike.new
-		bike.break!
+		#bike.break!
+		breaking
 		expect(bike).to be_broken
 	end
 
 	it "should be able to be fixed" do
-		bike = Bike.new
-		bike.break!
+		#bike.break!
+		breaking
 		bike.fix!
 		expect(bike).not_to be_broken
 	end
