c89355c5519e12fe86bb9d547a41468f530d98eb
Andy <andygnewman@gmail.com>
2014-12-09 12:39:45 +0000

    initial spec file

---
+++ spec/bike_spec.rb
@@ -0,0 +1,11 @@
+#we're describing the functionality of a specific class, Bike
+describe Bike do
+	# this is a specific feature (behaviour)
+	# that we expect to be present
+	it "should not be broken after we create it" do
+		the_bike = Bike.new # initialising a new object of Bike class
+		# expect an instance of this class to have
+		# a method "broken?" that should return false
+		expect(the_bike).not_to be_broken
+	end
+end
